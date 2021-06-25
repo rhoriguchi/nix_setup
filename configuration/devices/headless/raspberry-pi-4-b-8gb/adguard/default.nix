6b5ffecca8a1bf5979ee1a3e55a8bb9f3b65fcfb
Ryan Horiguchi <ryan.horiguchi@gmail.com>
2021-06-25 14:49:15 +0200

    Add nginx for adguard

--- configuration/devices/headless/raspberry-pi-4-b-8gb/adguard/default.nix
+++ configuration/devices/headless/raspberry-pi-4-b-8gb/adguard/default.nix
@@ -3,8 +3,20 @@
 
   networking.hostName = "XXLPitu-AdGuard";
 
-  # TODO create initial config yaml and than create with nix
-  services.adguardhome.enable = true;
+  services = {
+    nginx = {
+      virtualHosts."0.0.0.0" = {
+        forceSSL = true;
+        enableACME = true;
+
+        locations."/".proxyPass = "http://127.0.0.1:3000";
+        basicAuth.admin = (import ../../../../secrets.nix).services.adguard.password;
+      };
+    };
+
+    # TODO create initial config yaml and than create with nix
+    adguardhome.enable = true;
+  };
 
   networking.firewall = {
     allowedTCPPorts = [ 53 3000 ];
